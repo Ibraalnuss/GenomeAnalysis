31845a690d327cf5396a2efa0b3b5d67332bb4a5
Ibraalnuss <ibra818@gmail.com>
2025-05-20 09:04:38 +0200

    Updated fastqc_job.sh

--- fastqc_job.sh
+++ fastqc_job.sh
@@ -1,32 +1,36 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2025-3-3            # Project/Account name
-#SBATCH -M snowy                     # Cluster name (if required)
-#SBATCH -p core                      # Partition/queue
-#SBATCH -n 2                         # Number of cores
-#SBATCH -t 00:30:00                  # Time limit (HH:MM:SS)
-#SBATCH -J fastqc_job                # Job name
-#SBATCH --mail-type=ALL              # Mail notifications for job events
-#SBATCH --mail-user=your_email@example.com  # Replace with your actual email
-#SBATCH --output=fastqc_job.%j.out   # Output file name (%j will be replaced by the job id)
+#SBATCH -A uppmax2025-3-3            # Project/account
+#SBATCH -M snowy                     # Cluster
+#SBATCH -p core                      # Queue
+#SBATCH -n 4                         # Cores
+#SBATCH -t 00:30:00                  # Walltime
+#SBATCH -J fastqc_rna                # Job name
+#SBATCH --mail-type=END,FAIL         # Only mail on completion/failure
+#SBATCH --mail-user=you@your.email   
+#SBATCH --output=fastqc_rna.%j.out   # STDOUT (%j = jobID)
 
-module load bioinfo-tools  
+module purge
+module load bioinfo-tools
 module load FastQC
 
-FASTQC_EXEC="/proj/uppmax2025-3-3/private/FastQC/fastqc"
-OUTPUT_DIR="/proj/uppmax2025-3-3/private/Efaecium_Project/analysis/qc/fastqc_raw"
-ILLUMINA_DIR="/proj/uppmax2025-3-3/Genome_Analysis/1_Zhang_2017/genomics_data/Illumina"
-PACBIO_DIR="/proj/uppmax2025-3-3/Genome_Analysis/1_Zhang_2017/genomics_data/PacBio"
-NANOPORE_DIR="/proj/uppmax2025-3-3/Genome_Analysis/1_Zhang_2017/genomics_data/Nanopore"
+OUTPUT_DIR="/proj/uppmax2025-3-3/private/Efaecium_Project/analysis/qc/fastqc_rn$
+mkdir -p "${OUTPUT_DIR}"
 
+DIR1="/proj/uppmax2025-3-3/Genome_Analysis/1_Zhang_2017/transcriptomics_data/RN$
+DIR2="/proj/uppmax2025-3-3/Genome_Analysis/1_Zhang_2017/transcriptomics_data/RN$
 
-echo "Starting FastQC on Illumina data..."
-${FASTQC_EXEC} -o ${OUTPUT_DIR} ${ILLUMINA_DIR}/*.fq.gz
+echo "[$(date)] FastQC on RNA‑Seq (Serum) …"
+fastqc -t 4 -o "${OUTPUT_DIR}" \
+  "${DIR1}"/trim_paired_*_pass_1.fastq.gz \
+  "${DIR1}"/trim_paired_*_pass_2.fastq.gz \
+  "${DIR1}"/trim_single_*_pass_1.fastq.gz \
+  "${DIR1}"/trim_single_*_pass_2.fastq.gz
 
-echo "Starting FastQC on PacBio data..."
-${FASTQC_EXEC} -o ${OUTPUT_DIR} ${PACBIO_DIR}/*.fastq.gz
+echo "[$(date)] FastQC on RNA‑Seq (BH) …"
+fastqc -t 4 -o "${OUTPUT_DIR}" \
+  "${DIR2}"/trim_paired_*_pass_1.fastq.gz \
+  "${DIR2}"/trim_paired_*_pass_2.fastq.gz \
+  "${DIR2}"/trim_single_*_pass_1.fastq.gz \
+  "${DIR2}"/trim_single_*_pass_2.fastq.gz
 
-echo "Starting FastQC on Nanopore data (FASTA format; quality metrics will be limited)..."
-${FASTQC_EXEC} -o ${OUTPUT_DIR} ${NANOPORE_DIR}/*.fasta.gz
-
-echo "FastQC analysis complete."
-
+echo "[$(date)] FastQC complete; reports in ${OUTPUT_DIR}"
